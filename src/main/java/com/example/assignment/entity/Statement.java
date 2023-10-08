610fb5fc6e7e2a76596c8e9836bdded3330a58ec
jbeli <jbelikhaoula8@gmail.com>
2023-10-08 12:28:09 +0300

    updates

--- src/main/java/com/example/assignment/entity/Statement.java
+++ src/main/java/com/example/assignment/entity/Statement.java
@@ -6,7 +6,6 @@ import java.security.NoSuchAlgorithmException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.example.assignment.repository.StatementRepositoryImpl;
 
 public class Statement {
     private static final Logger logger = LoggerFactory.getLogger(Statement.class);
